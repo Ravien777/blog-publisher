7605d35f8a7c6728ff5420aaa06905d61c7aa573
Ravien777 <raviensewpal@gmail.com>
2026-04-28 08:54:02 -0300

    Add electron builder

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 900,
-    height: 600,
+    width: 1200,
+    height: 700,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
